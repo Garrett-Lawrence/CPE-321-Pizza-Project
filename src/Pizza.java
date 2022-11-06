24ff4dd3644eae684f2824466b5863dcb4d0b41c
Bailey <bnw0023@uah.edu>
2022-11-06 13:01:34 -0600

    Update Pizza Class

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+/**
+ * Pizza contains toppings, crust, sizes, sauces
+ * that can be added/removed for Customers order.
+ */
 public class Pizza {
+    private ToppingsList pizzaToppings;
+    private Crust pizzaCrust;
+    private Size pizzaSize;
+    private Sauce pizzaSauce;
+    private double pizzaPrice;
+    /**
+     * Constructor Pizza
+     */
+    public Pizza(ToppingsList toppings, Crust crust, Size size, Sauce sauce){
+        pizzaToppings = toppings;
+        pizzaCrust = crust;
+        pizzaSauce = sauce;
+        pizzaSize = size;
+    }
+
+    public double getPizzaPrice(){
+        pizzaPrice = pizzaToppings.getToppingsPrice() + pizzaCrust.getCrustPrice() +
+                pizzaSauce.getSaucePrice() + pizzaSize.getSizePrice();
+        return pizzaPrice;
+    }
 }
