8b45e28f982635bcb1b5c33a4565a8b4c1c70f9d
Bailey <bnw0023@uah.edu>
2022-11-07 10:13:06 -0600

    Update Pizza Class

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 /**
  * Pizza contains toppings, crust, sizes, sauces
  * that can be added/removed for Customers order.
@@ -23,5 +22,23 @@ public class Pizza {
         pizzaPrice = pizzaToppings.getToppingsPrice() + pizzaCrust.getCrustPrice() +
                 pizzaSauce.getSaucePrice() + pizzaSize.getSizePrice();
         return pizzaPrice;
+    }
+
+    public void changeItem(String itemType, String newItem, double price) {
+        if (itemType == "Crust"){
+            pizzaCrust = new Crust(newItem, price);
+        } else if (itemType == "Sauce") {
+            pizzaSauce = new Sauce(newItem, price);
+        } else if (itemType == "Sauce") {
+            pizzaSize = new Size(newItem);
+        }
+    }
+
+    public void addTopping(Topping topping){
+        pizzaToppings.add(topping);
+    }
+
+    public void removeTopping(Topping topping){
+        pizzaToppings.remove(topping);
     }
 }
