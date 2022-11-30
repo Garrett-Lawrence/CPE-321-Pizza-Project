b8d78ddbbe381eb293e08c904d208a7be92950da
Bailey <bnw0023@uah.edu>
2022-11-30 10:27:56 -0600

    GUI set up (First Window, Second Window, Custom Menu)

--- src/SecondWindow.java
+++ src/SecondWindow.java
@@ -1,14 +1,6 @@
 import javax.swing.*;
+
 public class SecondWindow {
-    public SecondWindow() {
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 1000);
-        JLabel label = new JLabel("You can order from our Popular Menu or Custom Pizza Menu!");
-        JButton button1 = new JButton("Popular Menu");
-        frame.getContentPane().add(button1);
-        JButton button2 = new JButton("Custom Pizza Menu");
-        frame.getContentPane().add(button2);
-        frame.setVisible(true);
-    }
+    private JButton popularMenuButton;
+    private JButton customMenuButton;
 }
