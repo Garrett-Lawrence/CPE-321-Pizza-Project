50450c302aa7634785550ae329837033280ffd9e
Bailey <bnw0023@uah.edu>
2022-11-30 10:47:01 -0600

    GUI set up (Popular Menu)

--- src/PopularMenu.java
+++ src/PopularMenu.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
+
 public class PopularMenu {
-    public PopularMenu(){
-        JFrame frame = new JFrame("Charger Pizza");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 1000);
-        JLabel label = new JLabel("Welcome to Charger Pizza!");
-        JButton button = new JButton("Cancel");
-        frame.getContentPane().add(button);
-        frame.setVisible(true);
-    }
+    private JPanel panel1;
+    private JScrollBar scrollBar1;
+    private JRadioButton smallRadioButton;
+    private JRadioButton mediumRadioButton;
+    private JRadioButton largeRadioButton;
+    private JRadioButton smallRadioButton1;
+    private JRadioButton mediumRadioButton1;
+    private JRadioButton largeRadioButton1;
+    private JRadioButton smallRadioButton2;
+    private JRadioButton mediumRadioButton2;
+    private JRadioButton largeRadioButton2;
 }
