b8ba9c4cd7b8e5afeb1be2a0e0e4a19b81b72106
NavGoHam <arnav.maroju@gmail.com>
2022-12-01 20:40:17 -0600

    toppings list edits

--- src/ToppingsList.java
+++ src/ToppingsList.java
@@ -8,27 +8,27 @@ public class ToppingsList {
     private double toppingsPrice;
     public ToppingsList(){
         toppingsList = new ArrayList<>() ;
-    }
+    }   // constructor creates new arraylist for toppings
 
-    public void remove(Topping topping){
+    public void remove(Topping topping){                           // void function that does not need to return anything; just removes the topping in desired index
         int index = toppingsList.indexOf(topping);
         toppingsList.remove(index);
     }
 
     public void add(Topping newTopping){
         toppingsList.add(newTopping);
-    }
+    }  // function to add new topping to array list
 
     public int size(){
         return toppingsList.size();
-    }
+    }                   // returns size of arraylist
 
-    public double getToppingsPrice(){
+    public double getToppingsPrice(){                                   // gets toppings price array list (toppingsList)
         double sum = 0;
         for (int i = 0; i < toppingsList.size(); i++){
-            sum += toppingsList.get(i).getToppingPrice();
+            sum += toppingsList.get(i).getToppingPrice();           // for every index in the array list, sums up the topping price by getting the price of each topping
         }
         toppingsPrice = sum;
-        return sum;
+        return sum;                                                 // returns topping price in variable sum
     }
 }
