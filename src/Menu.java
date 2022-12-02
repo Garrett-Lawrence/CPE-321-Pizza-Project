0d32bb7c2869919f6b26b44b19845a0b434e317b
NavGoHam <arnav.maroju@gmail.com>
2022-12-01 20:26:20 -0600

    Menu class shizzle !!

--- src/Menu.java
+++ src/Menu.java
@@ -9,10 +9,10 @@ import java.util.Hashtable;
 public class Menu {
     public Dictionary<String,String> toppingPrices;
     public Dictionary<String,String> crustPrices;
-    public Dictionary<String,String> saucePrices;
-    public final String[] sizeList = {"Small", "Medium", "Large"};
+    public Dictionary<String,String> saucePrices;                   // Initializing dictionaries to store key value pairs for topping, crust, and sauce prices.
+    public final String[] sizeList = {"Small", "Medium", "Large"};  // 3 different types of pizza sizes
 
-    public Menu(){
+    public Menu(){                                                  // Constructor to initialize hash tables
         toppingPrices = new Hashtable<>();
         crustPrices = new Hashtable<>();
         saucePrices = new Hashtable<>();
@@ -20,15 +20,15 @@ public class Menu {
 
     public void addToppingToMenu(String key, String value){
         toppingPrices.put(key,value);
-    }
+    } // function to add toppings and prices
 
     public void removeToppingFromMenu(String key){
         toppingPrices.remove(key);
-    }
+    } // function to remove the topping
 
     public void addCrustToMenu(String key, String value){
         crustPrices.put(key,value);
-    }
+    } // function to add crust and prices to menu
 
     public void removeCrustFromMenu(String key){
         crustPrices.remove(key);
@@ -42,17 +42,17 @@ public class Menu {
         saucePrices.remove(key);
     }
 
-    public double returnToppingPrice(String key){
+    public double returnToppingPrice(String key){                          // function that gets thr topping price then returns it as double
         String value = toppingPrices.get(key);
         return Double.parseDouble(value);
     }
 
-    public double returnCrustPrice(String key){
+    public double returnCrustPrice(String key){                           // function that gets the crust price then returns it as double
         String value = crustPrices.get(key);
         return Double.parseDouble(value);
     }
 
-    public double returnSaucePrice(String key){
+    public double returnSaucePrice(String key){                           // function that gets the sauce price then returns it as double
         String value = saucePrices.get(key);
         return Double.parseDouble(value);
     }
