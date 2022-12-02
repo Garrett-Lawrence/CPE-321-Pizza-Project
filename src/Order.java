47b8502a56f1801c631e7b5673342a1c36f69026
VictoriaTarpley <victarpley@gmail.com>
2022-12-01 20:46:31 -0600

    updating Order class

--- src/Order.java
+++ src/Order.java
@@ -1,24 +1,33 @@
+/**
+ * Order class stores the list of pizzas in the current order and the orderPrice
+ */
 import java.util.ArrayList;
-
 public class Order {
 
+    // array to hold list of pizzas in order
     private ArrayList<Pizza> pizzaList;
+    // total order price
     private double orderPrice;
 
+    // constructors for Order that just creates new empty pizzaList
     public Order(){
         pizzaList = new ArrayList<>() ;
     }
 
+    // add Pizza to the pizzaList
     public void addPizzaToOrder(Pizza newPizza){
         this.pizzaList.add(newPizza);
     }
 
+    // returning the size of pizzaList for number of pizzas
     public int getPizzaCount() {
         return this.pizzaList.size();
     }
 
+    // function that returns total price of order
     public double getOrderPrice(){
         double sum = 0;
+        // loops through each item in pizza list and adds together the prices
         for (int i = 0; i < this.pizzaList.size(); i++){
             sum += this.pizzaList.get(i).getPizzaPrice();
         }
