64cbfbab93545d5559c3760adba6c79c31a0de50
NavGoHam <arnav.maroju@gmail.com>
2022-12-01 19:29:44 -0600

    custom buttons go crazy

--- src/CustomMenu.java
+++ src/CustomMenu.java
@@ -89,7 +89,7 @@ public class CustomMenu extends JDialog {
         boolean crustCheck = false;
         boolean sauceCheck = false;
         if (pepperoniCheckBox.isSelected()) {
-            Topping pepperoni = new Topping("Pepperoni", 0.30);
+            Topping pepperoni = new Topping("Pepperoni", 0.50);
             toppingsList.add(pepperoni);
         }
         if (sausageCheckBox.isSelected()) {
@@ -117,18 +117,61 @@ public class CustomMenu extends JDialog {
             toppingsList.add(olives);
         }
         if (onionsCheckBox.isSelected()) {
-            Topping onions = new Topping("Onions", );
+            Topping onions = new Topping("Onions", 0.25);
             toppingsList.add(onions);
         }
         if (extraCheeseCheckBox.isSelected()) {
-            Topping cheese = new Topping("Cheese", );
+            Topping cheese = new Topping("Cheese", 0.40);
             toppingsList.add(cheese);
         }
-
-        dispose();
-        Checkout checkout = new Checkout();
-        checkout.pack();
-        checkout.setVisible(true);
+        if (smallRadioButton.isSelected()) {
+            size = new Size("Small");
+        }
+        else if (mediumRadioButton.isSelected()){
+            size = new Size("Medium");
+        }
+        else if (largeRadioButton.isSelected()) {
+            size = new Size("Large");
+        }
+        else {
+            sizeCheck = false;
+            ErrorWindow errorWindow = new ErrorWindow();
+            errorWindow.pack();
+            errorWindow.setVisible(true);
+        }
+        if (originalCrustButton.isSelected()) {
+            crust = new Crust("Original", 0.0);
+        }
+        else if (stuffedCrustButton.isSelected()) {
+            crust = new Crust("Stuffed", 1.50);
+        }
+        else if (garlicCrustButton.isSelected()) {
+            crust = new Crust("Garlic", 1.00);
+        }
+        else {
+            crustCheck = false;
+            ErrorWindow errorWindow = new ErrorWindow();
+            errorWindow.pack();
+            errorWindow.setVisible(true);
+        }
+        if (originalSauceButton.isSelected()) {
+            sauce = new Sauce("Original", 0.0);
+        }
+        else if (oliveOilSauceButton.isSelected()) {
+            sauce = new Sauce("Olive Oil", 0.25);
+        }
+        else {
+            sauceCheck =false;
+            ErrorWindow errorWindow = new ErrorWindow();
+            errorWindow.pack();
+            errorWindow.setVisible(true);
+        }
+        if (sauceCheck == true && crustCheck == true && sizeCheck == true) {
+            dispose();
+            Checkout checkout = new Checkout();
+            checkout.pack();
+            checkout.setVisible(true);
+        }
     }
 
     private void onAddAnother() {
