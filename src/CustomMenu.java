791f1311e530da2e0bf75b35d574336ed0b9464e
VictoriaTarpley <victarpley@gmail.com>
2022-12-01 19:46:08 -0600

    testing them heifers

--- src/CustomMenu.java
+++ src/CustomMenu.java
@@ -85,9 +85,98 @@ public class CustomMenu extends JDialog {
     }
 
     private void onCheckout() {
-        boolean sizeCheck = false;
-        boolean crustCheck = false;
-        boolean sauceCheck = false;
+        boolean sizeCheck = true;
+        boolean crustCheck = true;
+        boolean sauceCheck = true;
+        if (pepperoniCheckBox.isSelected()) {
+            Topping pepperoni = new Topping("Pepperoni", 0.50);
+            toppingsList.add(pepperoni);
+        }
+        if (sausageCheckBox.isSelected()) {
+            Topping sausage = new Topping("Sausage", 0.40);
+            toppingsList.add(sausage);
+        }
+        if (tomatoesCheckBox.isSelected()) {
+            Topping tomato = new Topping("Tomatoes", 0.10);
+            toppingsList.add(tomato);
+        }
+        if (peppersCheckBox.isSelected()) {
+            Topping peppers = new Topping("Green Peppers", 0.20);
+            toppingsList.add(peppers);
+        }
+        if (pineappleCheckBox.isSelected()) {
+            Topping pineapple = new Topping("Pineapple", 0.30);
+            toppingsList.add(pineapple);
+        }
+        if (mushroomCheckBox.isSelected()) {
+            Topping mushroom = new Topping("Mushrooms", 0.30);
+            toppingsList.add(mushroom);
+        }
+        if (olivesCheckBox.isSelected()) {
+            Topping olives = new Topping("Olives", 0.15);
+            toppingsList.add(olives);
+        }
+        if (onionsCheckBox.isSelected()) {
+            Topping onions = new Topping("Onions", 0.25);
+            toppingsList.add(onions);
+        }
+        if (extraCheeseCheckBox.isSelected()) {
+            Topping cheese = new Topping("Cheese", 0.40);
+            toppingsList.add(cheese);
+        }
+        if (true) {
+            if (smallRadioButton.isSelected()) {
+                size = new Size("Small");
+            } else if (mediumRadioButton.isSelected()) {
+                size = new Size("Medium");
+            } else if (largeRadioButton.isSelected()) {
+                size = new Size("Large");
+            } else {
+                sizeCheck = false;
+                ErrorWindow errorWindow = new ErrorWindow();
+                errorWindow.pack();
+                errorWindow.setVisible(true);
+            }
+        }
+        if (true) {
+            if (originalCrustButton.isSelected()) {
+                crust = new Crust("Original", 0.0);
+            } else if (stuffedCrustButton.isSelected()) {
+                crust = new Crust("Stuffed", 1.50);
+            } else if (garlicCrustButton.isSelected()) {
+                crust = new Crust("Garlic", 1.00);
+            } else {
+                crustCheck = false;
+                ErrorWindow errorWindow = new ErrorWindow();
+                errorWindow.pack();
+                errorWindow.setVisible(true);
+            }
+        }
+        if (true) {
+            if (originalSauceButton.isSelected()) {
+                sauce = new Sauce("Original", 0.0);
+            } else if (oliveOilSauceButton.isSelected()) {
+                sauce = new Sauce("Olive Oil", 0.25);
+            } else {
+                sauceCheck = false;
+                ErrorWindow errorWindow = new ErrorWindow();
+                errorWindow.pack();
+                errorWindow.setVisible(true);
+            }
+        }
+        if (sauceCheck == true && crustCheck == true && sizeCheck == true) {
+            customPizza = new Pizza(toppingsList, crust, sauce, size);
+            dispose();
+            Checkout checkout = new Checkout();
+            checkout.pack();
+            checkout.setVisible(true);
+        }
+    }
+
+    private void onAddAnother() {
+        boolean sizeCheck = true;
+        boolean crustCheck = true;
+        boolean sauceCheck = true;
         if (pepperoniCheckBox.isSelected()) {
             Topping pepperoni = new Topping("Pepperoni", 0.50);
             toppingsList.add(pepperoni);
@@ -169,17 +258,10 @@ public class CustomMenu extends JDialog {
         if (sauceCheck == true && crustCheck == true && sizeCheck == true) {
             customPizza = new Pizza(toppingsList, crust, sauce, size);
             dispose();
-            Checkout checkout = new Checkout();
-            checkout.pack();
-            checkout.setVisible(true);
+            SecondWindow secondWindow = new SecondWindow();
+            secondWindow.pack();
+            secondWindow.setVisible(true);
         }
-    }
-
-    private void onAddAnother() {
-        dispose();
-        SecondWindow secondWindow = new SecondWindow();
-        secondWindow.pack();
-        secondWindow.setVisible(true);
     }
 
     private void onCancel() {
