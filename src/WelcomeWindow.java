aa5372f0590529a7a81fc94efad4ff07332bf2dc
VictoriaTarpley <victarpley@gmail.com>
2022-12-02 13:51:21 -0600

    updating WelcomeWindow

--- src/WelcomeWindow.java
+++ src/WelcomeWindow.java
@@ -1,3 +1,7 @@
+/**
+ * Welcome Window class opens up the welcome window display that allows user to Start Order of Cancel
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -7,7 +11,9 @@ public class WelcomeWindow extends JDialog {
     private JButton buttonOK;
     private JButton buttonCancel;
 
+    // Constructor for creating welcome window
     public WelcomeWindow() {
+        // Set up and sizing of window
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (int) (dimension.getWidth()/2);
         int y = (int) (dimension.getHeight()/2);
@@ -16,12 +22,14 @@ public class WelcomeWindow extends JDialog {
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
+        // if ok button is clicked, call onStartOrder function
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onStartOrder();
             }
         });
 
+        // if cancel button is clicked, call onCancel function
         buttonCancel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
@@ -44,6 +52,7 @@ public class WelcomeWindow extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
+    // onStartOrder creates a new order from the Order class, opens up the second window, and closes WelcomeWindow
     private void onStartOrder() {
         Order order  = new Order();
         SecondWindow secondWindow = new SecondWindow(order);
@@ -52,11 +61,13 @@ public class WelcomeWindow extends JDialog {
         dispose();
     }
 
+    // onCancel closes WelcomeWindow
     private void onCancel() {
         // add your code here if necessary
         dispose();
     }
 
+    // Main function that opens up the first Window of the program, Welcome Window
     public static void main(String[] args) {
         WelcomeWindow dialog = new WelcomeWindow();
         dialog.pack();
