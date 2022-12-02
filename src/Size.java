6adc5ff081a6e96bda35c8f31aad937cc0c2e8fa
NavGoHam <arnav.maroju@gmail.com>
2022-12-01 18:58:37 -0600

    Size class

--- src/Size.java
+++ src/Size.java
@@ -1,19 +1,19 @@
 
-public class Size {
+public class Size {     // Size class to manage price according to chosen size
     private String size;
     private double sizePrice;
 
-    public Size(String name){
+    public Size(String name){ // constructor to initialize size
         size = name;
         if (size == "Small")
             sizePrice = 10.00;
         else if (size == "Medium")
             sizePrice = 12.50;
-        else if (size == "Large")
+        else if (size == "Large") // if else chain to assign a base price to pizza based on chosen size
             sizePrice = 15.00;
     }
 
     public double getSizePrice(){
         return sizePrice;
-    }
+    } // function to return sizePrice
 }
